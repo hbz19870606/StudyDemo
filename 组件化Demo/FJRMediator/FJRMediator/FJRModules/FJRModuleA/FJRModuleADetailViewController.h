60bd3e3659d30d5953055201f95e0a27a35de880
胡大海 <dahai.hu@fengjr.com>
2016-06-04 16:25:42 +0800

    组件化-casa
    组件化Demo

---
+++ 组件化Demo/FJRMediator/FJRMediator/FJRModules/FJRModuleA/FJRModuleADetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  FJRModuleADetailViewController.h
+//  FJRMediator
+//
+//  Created by 胡大海 on 16/6/4.
+//  Copyright © 2016年 胡大海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FJRModuleADetailViewController : UIViewController
+
+@property (nonatomic, strong, readonly) UILabel *valueLabel;
+@property (nonatomic, strong, readonly) UIImageView *imageView;
+
+@end
